e94b6561da56c3653ab1568762ae55bd0c5396c3
Daniel Walker <dw765608@gmail.com>
2021-06-03 16:23:28 -0400

    wip

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const server = express()
 require('dotenv').config()
 server.use(express.json())
-const port = 5000
+const port = process.env.PORT||4000
 
 if(process.env.NODE_ENV==="development"){
     console.log(process.env.NODE_ENV)

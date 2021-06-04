c2e57d7a16f13019af72124c72ac821560be08ee
Daniel Walker <dw765608@gmail.com>
2021-06-04 19:50:22 -0400

    wipnpm commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,25 @@
-import logo from './logo.svg';
-import react,{useEffect,useState} from 'react'
+
 import './App.css';
-import axios from 'axios'
+
 function App() {
-  const [state,setState] = useState(null)
-  useEffect(()=>{
-    axios.get("/api/posts")
-    .then(response =>{
-      console.log(response)
-    })
-    .catch(err =>{
-      console.log(err)
-    })
-  },[])
+
+  // useEffect(()=>{
+  //   axios.get("http://localhost:5000/api/posts")
+  //   .then(response =>{
+  //     console.log(response)
+  //     setState(response)
+  //   })
+  //   .catch(err =>{
+  //     console.log(err)
+ //   })
+ // },[])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* {! state ? <h1>loading...</h1> :state.data.map(data =>{
+        return (<>{data.title}</>)
+      })} */}
+      {"hello"}
       </header>
     </div>
   );

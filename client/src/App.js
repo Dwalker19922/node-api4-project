584c88a3106c411c02362acfc7ebd05e5117c3c7
Daniel Walker <dw765608@gmail.com>
2021-06-04 20:01:43 -0400

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,9 @@ import './App.css';
 
 function App() {
 
-  // useEffect(()=>{
-  //   axios.get("http://localhost:5000/api/posts")
-  //   .then(response =>{
-  //     console.log(response)
-  //     setState(response)
-  //   })
-  //   .catch(err =>{
-  //     console.log(err)
- //   })
- // },[])
   return (
     <div className="App">
       <header className="App-header">
-      {/* {! state ? <h1>loading...</h1> :state.data.map(data =>{
-        return (<>{data.title}</>)
-      })} */}
       {"hello"}
       </header>
     </div>
